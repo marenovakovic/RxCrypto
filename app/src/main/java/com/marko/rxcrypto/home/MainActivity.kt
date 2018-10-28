40e8fcc7bb789310f936e40c546b53a3ee11dd85
Marko <graornovakovic@gmail.com>
2018-10-28 17:50:14 +0100

    up navigation

--- app/src/main/java/com/marko/rxcrypto/home/MainActivity.kt
+++ app/src/main/java/com/marko/rxcrypto/home/MainActivity.kt
@@ -2,10 +2,13 @@ package com.marko.rxcrypto.home
 
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.marko.presentation.coins.CoinsViewModel
 import com.marko.presentation.coins.CoinsViewModelFactory
 import com.marko.rxcrypto.R
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -23,5 +26,13 @@ class MainActivity : DaggerAppCompatActivity() {
 
 		// init ViewModel so fragments can use it
 		viewModel
+
+		NavigationUI.setupActionBarWithNavController(
+			this,
+			NavHostFragment.findNavController(mainNavHostFragment)
+		)
 	}
+
+	override fun onSupportNavigateUp() =
+		NavHostFragment.findNavController(mainNavHostFragment).navigateUp()
 }
